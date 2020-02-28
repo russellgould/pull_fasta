cdc3718479ec024e97bbc6a1a480d5cf774a96db
Russell Gould <gouldru@oregonstate.edu>
2020-02-28 14:05:35 -0800

    add reference argument

--- pull_fasta/main.py
+++ pull_fasta/main.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser, FileType
+from pathlib import Path
 from numpy import where
 from .utils import *
 import subprocess
@@ -8,7 +9,8 @@ group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-gff", action="store_true", help="indicate the input file is GFF")
 group.add_argument("-bed", action="store_true", help="indicate the input file is BED")
 group.add_argument("-peak", action="store_true", help="indicate the input file is PEAK")
-parser.add_argument("input", type=FileType("r"), help="file containing regions")
+parser.add_argument("regions", type=FileType("r"), help="file containing regions")
+parser.add_argument("reference", type=Path, help="reference fasta file", dest="ref")
 parser.add_argument(
     "-nu",
     "--nucs_up",
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     bedtools_cols = ["chrom", "start", "end", "name", "score", "strand"]
 
     if args.peak:
-        regions = read_peak(args.input)
+        regions = read_peak(args.regions)
         regions["start"] = where(
             regions["strand"] == "-",
             regions["loc"] - args.nucs_down - 1,
@@ -45,14 +47,21 @@ if __name__ == "__main__":
         regions["score"] = 0
     else:
         if args.gff:
-            regions = read_gff(args.input)
+            regions = read_gff(args.regions)
             regions["start"] -= 1
             if "name" not in regions.columns:
                 regions["name"] = "."
             regions = regions[bedtools_cols]
         else:
-            regions = read_bed(args.input)
+            regions = read_bed(args.regions)
 
     regions = regions[bedtools_cols]
 
-    print(regions)
+    tmp_path = Path("tmp.bed").resolve()
+    regions.to_csv(tmp_path, sep="\t", index=False, header=False)
+
+    cmd = subprocess.run(
+        ["bedtools", "getfasta", "-fi", args.ref, "-bed", tmp_path, "-s"]
+    )
+
+    print(cmd.stdout)
