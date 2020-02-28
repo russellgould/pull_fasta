9338bdd2619dcd8f05b004c6260e8c0a76645468
Russell Gould <gouldru@oregonstate.edu>
2020-02-28 12:50:43 -0800

    add test for exit status

--- tests/test_pull_fasta.py
+++ tests/test_pull_fasta.py
@@ -1,5 +1,19 @@
-from pull_fasta import __version__
+import pytest
+import pull_fasta as app
+import subprocess
+from pathlib import Path
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert app.__version__ == "0.1.0"
+
+
+def test_exit_status():
+    cmd = subprocess.run(["python", "-m", "pull_fasta.main", "-h"])
+    assert cmd.check_returncode
+
+
+def test_peak():
+    infile = Path("/Users/user/Documents/Lab/pull_fasta/tests/data/regions.peaks")
+    cmd = subprocess.run(["python", "-m", "pull_fasta.main", "-peak", infile])
+
