d22d7555022e7dac9e4740b918641da0ffaf3f92
Russell Gould <gouldru@oregonstate.edu>
2020-03-03 12:20:04 -0800

    add link to grouper def

--- pull_fasta/main.py
+++ pull_fasta/main.py
@@ -9,8 +9,9 @@ from itertools import zip_longest
 
 
 def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    """Collect data into fixed-length chunks or blocks
+    Taken from https://docs.python.org/3/library/itertools.html#itertools-recipes
+    """
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
 
